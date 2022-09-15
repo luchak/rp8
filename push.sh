3a2fd14ee5a18ee8241f361976d0f6d69be10746
luchak <luchak@gmail.com>
2022-09-15 00:11:38 -0700

    Add push script

---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ex
+
+butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_windows.zip luchak/rp8:win
+butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_osx.zip luchak/rp8:mac
+butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_linux.zip luchak/rp8:linux
+butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_raspi.zip luchak/rp8:raspi
