0bc99317e3fcb12d97998e1a061e8843cd31fcfc
luchak <luchak@gmail.com>
2022-09-16 19:58:36 -0700

    Fix up push and export scripts

--- bin/export.sh
+++ bin/export.sh
@@ -5,7 +5,7 @@ pico8 -export /tmp/rp8.html "$(grealpath ./build/rp8_min.p8.png)"
 pico8 -export /tmp/rp8.bin "$(grealpath ./build/rp8_min.p8.png)"
 mv /tmp/rp8.html /tmp/rp8.js ./exports/html
 mv ./exports/html/rp8.html ./exports/html/index.html
-rm -r ./exports/rp8.bin
+rm -rf ./exports/rp8.bin
 cp -r /tmp/rp8.bin ./exports
-rm ./exports/rp8_html.zip
+rm -f ./exports/rp8_html.zip
 zip -j ./exports/rp8_html.zip exports/html/index.html exports/html/rp8.js
--- bin/push.sh
+++ bin/push.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_windows.zip luchak/rp8:win
-butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_osx.zip luchak/rp8:mac
-butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_linux.zip luchak/rp8:linux
-butler push --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8_raspi.zip luchak/rp8:raspi
+echo $(pwd)
+
+butler push --auto-wrap --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/windows luchak/rp8:win
+butler push --auto-wrap --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/rp8.app luchak/rp8:mac
+butler push --auto-wrap --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/linux luchak/rp8:linux
+butler push --auto-wrap --fix-permissions --if-changed --userversion="$1" exports/rp8.bin/raspi luchak/rp8:raspi
 butler push --fix-permissions --if-changed --userversion="$1" docs/user_guide.pdf luchak/rp8:manual
